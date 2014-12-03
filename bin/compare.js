ed5bdb2ed1439c1547057459da925b7250acc4c5
Myles Borins <myles@famo.us>
2014-12-03 01:24:04 -0800

    All done for now

--- bin/compare.js
+++ bin/compare.js
@@ -3,8 +3,10 @@
 var path = require('path');
 
 var fs = require('vinyl-fs');
-var browserify = require('browserify');
 var map = require('map-stream');
+
+var browserify = require('browserify');
+var uglify = require('uglify-js');
 
 var filesize = require('filesize');
 var chalk = require('chalk');
@@ -15,10 +17,13 @@ var yellow = chalk.yellow;
 function build(file, cb) {
   var b = browserify();
   b.add(file.path);
+  b.transform('uglifyify');
   b.bundle(function (err, src) {
+    var minified = uglify.minify(src.toString(), {fromString: true});
     console.log([green('filename:'), path.basename(file.path)].join(' '));
-    console.log([yellow('Bundled size:'), filesize(src.length) + '\n'].join(' '));
-    cb(null, file);
+    console.log([yellow('Bundled size:'), filesize(src.length)].join(' '));
+    console.log([yellow('Minified size:'), filesize(minified.code.length) + '\n'].join(' '));
+    cb(null, src);
   });
 }
 
